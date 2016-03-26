f581f3bd615e1d604cab33cfaab5b44b7d6ea7a1
Robbin Torstensson <rotor00a@hotmail.com>
2016-03-26 12:43:46 +0100

    Added a folder and implemented a basic HP component

---
+++ AntJam/Assets/_Master/Code/HP/HP.cs
@@ -0,0 +1,30 @@
+﻿[System.Serializable]
+public class HP
+{
+    public int currentHp { get { return m_currentHp; } }
+    public int maxHp { get { return m_maxHp; } }
+    
+    public void DealDamage(int damage)
+    {
+        m_currentHp -= damage;
+
+        if (currentHp < 0)
+        {
+            m_currentHp = 0;
+        }
+    }
+
+    public void RestoreDamage(int restoredDamage)
+    {
+        m_currentHp += restoredDamage;
+
+        if (currentHp > maxHp)
+        {
+            m_currentHp = maxHp;
+        }
+    }
+    
+    private int m_currentHp;
+    [UnityEngine.SerializeField]
+    private int m_maxHp;
+}
---
+++ AntJam/Assets/_Master/Code/HP/HPComponent.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HPComponent : MonoBehaviour
+{
+    [SerializeField]
+    private HP m_hp;
+}
