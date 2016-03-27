23fb26f89a20912a2deeaf9c931e0e02c28d11ae
Gustaf Andersson <c.gustaf@hotmail.se>
2016-03-27 17:14:58 +0200

    Icons and ant balance

---
+++ AntJam/Assets/_Master/Code/UI/ButtonHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+public class ButtonHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Pointing at button.");
+        transform.Find("TooltipBG").gameObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.Find("TooltipBG").gameObject.SetActive(false);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed " + gameObject.ToString());
+        }
+	}
+}
