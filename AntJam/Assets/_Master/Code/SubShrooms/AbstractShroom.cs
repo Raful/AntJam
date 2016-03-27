0be821f7010c7d0f68ab8ef1ab5be5c2015511ab
Robbin Torstensson <rotor00a@hotmail.com>
2016-03-27 12:18:56 +0200

    Started implementing a shroom builder

--- AntJam/Assets/_Master/Code/SubShrooms/AbstractShroom.cs
+++ AntJam/Assets/_Master/Code/SubShrooms/AbstractShroom.cs
@@ -10,4 +10,28 @@ public abstract class AbstractShroom : MonoBehaviour
     private int m_Cost;
     [SerializeField][Tooltip("Construction cooldown in ms")]
     private int m_BuildCooldown;
+
+	public int cost { get { return m_Cost; } }
+	public int buildCooldown { get { return m_BuildCooldown; } }
+	public bool canBeBuilt 
+	{ 
+		get 
+		{
+			if (m_buildCooldownTimer >= 0 && m_buildCooldownTimer < m_BuildCooldown)
+				return false;
+
+			//TODO Check the cost
+
+			return true;
+		}
+	}
+
+	private float m_buildCooldownTimer = -1;
+
+	void LateUpdate() //Should be Update(), but I'm too lazy to fix compatibility with subclasses
+	{
+		//Update cooldown timer
+		if (m_buildCooldownTimer >= -0.0001f)
+			m_buildCooldownTimer += Time.deltaTime;
+	}
 }
